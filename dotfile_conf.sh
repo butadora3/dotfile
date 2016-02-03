3d0c83e90fcd8838d9aa55374d394e230bc415de
butadora3 <mail@butadora.com>
2016-02-03 23:51:22 +0900

    add pyenv configuration and bit bug fix

--- dotfile_conf.sh
+++ dotfile_conf.sh
@@ -3,10 +3,11 @@
 ln -sf ~/dotfile/_vimrc ~/.vimrc
 ln -sf ~/dotfile/_zshrc ~/.zshrc
 ln -sf ~/dotfile/vimfiles ~/.vim
-ln -sf ~/dotfile/pyenv ~/.pyenv
+#ln -sf ~/dotfile/pyenv ~/.pyenv
 
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/bundle
-#git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git submodule init
 git submodule update
