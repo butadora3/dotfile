2497a4ee36cdfd8ba2f321b00c93b5f004465513
butadora <masato_araki@toreta.in>
2021-06-21 21:54:09 +0900

    remove pyrnv and emacs

--- dotfile_conf.sh
+++ dotfile_conf.sh
@@ -7,7 +7,6 @@ ln -sf ~/dotfile/vimfiles ~/.vim
 
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/bundle
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git submodule init
 git submodule update
